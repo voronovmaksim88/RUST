e01b62127b3b5f8ca71b811aa59e769adbf42f55
voronovmaksim88 <voronovmaksim88@gmail.com>
2025-07-11 22:22:19 +0700

    Update main.rs
    добавил переменных

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -2,7 +2,20 @@ use std::io;
 
 fn main() {
     println!("Hello, Den from RUST!");
+
+    // Переменная (неизменяемая)
+    let number = 42;
     
+    // Выводим значение переменной
+    println!("Значение неизменяемой переменной: {}", number); 
+
+    // Переменная (изменяемая) типа i32
+    let mut var_num = 1;
+    var_num += 1;
+    
+    // Выводим значение переменной
+    println!("Значение переменной var_num: {}", var_num); 
+
     // Ожидаем ввода пользователя перед закрытием
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
